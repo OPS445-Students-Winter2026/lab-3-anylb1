8b4a93f394aadba5281b5fa5849a6ae176b17a10
Fonkouop Bless Anyl <fbless-anyl@myseneca.ca>
2026-02-06 10:34:53 -0500

    ops445 lab3 complete

---
+++ lab3d.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Author ID: fbless-anyl
+
+import subprocess
+
+def free_space():
+    p = subprocess.Popen(
+        "df -h | grep '/$' | awk '{print $4}'",
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True
+    )
+    output = p.communicate()
+    return output[0].decode('utf-8').strip()
+
+if __name__ == '__main__':
+    print(free_space())
